85655fa8a95d2b54b97aea32f59f6bd21b8cd51e
pstadler1990 <pstadler90@gmx.de>
2020-12-13 17:30:25 +0100

    first commit

---
+++ split_europarl.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# This script will split a single tsv file into 
+# two separate files with the first column being 
+# OUTPUT_FILE.src and the second column beging
+# INPUT_FILE.trg
+args=("$@")
+
+INPUT_FILE=${args[0]}
+OUTPUT_FILE=${args[1]}
+
+awk -F '\t' -v outname="$OUTPUT_FILE" 'BEGIN {file_src=outname ".src.csv"; file_trg=outname ".trg.csv"}; \
+    {print $1 >> file_src; print $2 >> file_trg}' $INPUT_FILE
