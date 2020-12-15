ed35da809302fb43ef51e4cf72bcb876a7bf6a55
pstadler1990 <pstadler90@gmx.de>
2020-12-15 14:09:58 +0100

    Change cut_europarl.sh to allow cutting by file or line count

--- cut_europarl.sh
+++ cut_europarl.sh
@@ -3,22 +3,34 @@ args=("$@")
 
 # This script cuts specified output files (.src, .trg)
 # into n smaller files (equally)
-# INPUT_FILE(S) argument1
-# CUT_NUMBER argument 2
-# Example: ./cut_europarl.sh europarl-en_de 10000
+# line / file mode
+# CUT_NUMBER argument 1
+# INPUT_FILE(S) argument 2
+# Example: ./cut_europarl.sh -l 10000 europarl-en_de    Cut into files with line length 10000
+# Example 2: ./cut_europarl.sh -f 100 europarl-en_de    Cut into 100 files 
 
-INPUT_FILE=${args[0]}
-CUT_NUMBER=${args[1]}
+INPUT_FILE=${args[2]}
 
-echo "Splitting $INPUT_FILE into $CUT_NUMBER files"
+mode=""
+while getopts ":f:l:" opt; do
+    case $opt in
+        f) mode="file"; CUT_NUMBER=$OPTARG; echo "ok file";
+        ;;
+        l) mode="line"; CUT_NUMBER=$OPTARG; echo "no line";
+        ;;
+        \?) echo "Error, invalid argument -$OPTARG" >&2; exit;
+        ;;
+    esac
+done
 
-mkdir cut_files
-mkdir cut_files/src
-mkdir cut_files/trg
+if [[ "$mode" == "file" ]]; then
+    echo "Splitting $INPUT_FILE into $CUT_NUMBER files"
+    INPUT_LINES=$(wc -l $INPUT_FILE | awk '{print $1}')
+    CALC_LINES_PER_FILE=$((INPUT_LINES / $CUT_NUMBER))
+    split -l $CALC_LINES_PER_FILE $INPUT_FILE
+else 
+    echo "Splitting $INPUT_FILE into files with $CUT_NUMBER lines"
+    split -l $CUT_NUMBER $INPUT_FILE
+fi
 
-split -l $CUT_NUMBER $INPUT_FILE.src 
-mv x* cut_files/src
-split -l $CUT_NUMBER $INPUT_FILE.trg
-mv x* cut_files/trg
-
-echo "Done, find the files in cut_files/src and cur_files/trg"
+echo "...done"
