04dcdcfb445d178152db9bc11629afaf3f05a0e8
pstadler1990 <pstadler90@gmx.de>
2020-12-29 00:17:10 +0100

    temporary changes

--- sockeye_scripts/prepare_devtest.sh
+++ sockeye_scripts/prepare_devtest.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 
-set -eu
-echo "Dir: $(dirname $0)"
 . $(dirname $0)/env.sh
-
-if [[ -z $4 ]]; then
-    echo "Usage: cat RAW_FILE | prepare.sh BPE_MODEL LANG PREFIX"
-    echo "  where BPE_MODEL is the path to the joint BPE model"
-    echo "        LANG is the ISO 639-1 two-character language code"
-    echo "        PREFIX is an output prefix"
-    echo "  Generates PREFIX.tok.LANG and PREFIX.bpe.LANG"
-    exit
-fi
 
 bpe_model=$1
 lang=$2
@@ -19,7 +8,7 @@ prefix=$3
 
 $MOSES/scripts/tokenizer/normalize-punctuation.perl -l $lang \
     | $MOSES/scripts/tokenizer/remove-non-printing-char.perl \
-    | $MOSES/scripts/tokenizer/tokenizer.perl -q -no-escape -protected $(dirname $0)/../tokenizer/basic-protected-patterns -l $lang \
+    | $MOSES/scripts/tokenizer/tokenizer.perl -q -no-escape -protected $MOSES/scripts/tokenizer/basic-protected-patterns -l $lang \
     | tee $prefix.tok.$lang \
-    | $BPE apply_bpe.py -c $bpe_model \
+    | python3 $BPE/apply_bpe.py -c $bpe_model \
     > $prefix.bpe.$lang
